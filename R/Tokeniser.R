eb1d82e05ebb846f5d6bf0acae9f3e604ede6320
Tim Churches <tim.churches@gmail.com>
2017-11-11 11:28:22 +1100

    Better example of the lexer

--- R/Tokeniser.R
+++ R/Tokeniser.R
@@ -37,5 +37,9 @@ while (lexer$lexpos <= lexer$lexlen) {
 lexer$input('this is a TEST, possibly a simplistic test, but not a SIMPLE test because it contains:\n2,2-bis(4-hydroxy-3-tert-butylphenyl)propane and 2,2-bis(4-tert-3-hydroxy-butylphenyl)propane amongst other things')
 
 while (lexer$lexpos <= lexer$lexlen) {
-  print(lexer$token())
+  tok <- lexer$token()
+  print(tok$type)
+  print(tok$value)
+  print(tok$lexpos)
+  print(tok)
 }
