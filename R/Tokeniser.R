bd682d1b6d7f2fd1b66f3522ee8238b0c38286dc
Tim Churches <tim.churches@gmail.com>
2017-11-11 11:10:47 +1100

    test line number tracking in Tokeniser.R

--- R/Tokeniser.R
+++ R/Tokeniser.R
@@ -33,3 +33,9 @@ lexer$input('this is a TEST, possibly a simplistic test, but not a SIMPLE test b
 while (lexer$lexpos <= lexer$lexlen) {
   print(lexer$token())
 }
+
+lexer$input('this is a TEST, possibly a simplistic test, but not a SIMPLE test because it contains:\n2,2-bis(4-hydroxy-3-tert-butylphenyl)propane and 2,2-bis(4-tert-3-hydroxy-butylphenyl)propane amongst other things')
+
+while (lexer$lexpos <= lexer$lexlen) {
+  print(lexer$token())
+}
