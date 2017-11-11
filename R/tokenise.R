c7d272b98756ecc6b259cba323403bbd6a0a2ffa
Tim Churches <tim.churches@gmail.com>
2017-11-11 11:06:42 +1100

    Add required library to tokenise.R

--- R/tokenise.R
+++ R/tokenise.R
@@ -1,3 +1,5 @@
+library(tidytext)
+
 delim_pattern <- "\\s+|[,!?\\.:;]\\s+"
 
 anzctr_core %>% select(trial_number, interventions) %>% unnest_tokens(word, interventions, token='regex', pattern=delim_pattern) %>% group_by(word) %>% count() %>% arrange(desc(n)) -> a
